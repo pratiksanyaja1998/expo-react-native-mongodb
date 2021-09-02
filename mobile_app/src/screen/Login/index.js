868637b5c4945e3bb3d9cb76787521e6ba8d9118
Pratik Sanyaja <pratiksanyaja1998@gmail.com>
2021-09-02 08:55:15 +0530

    working on login screen

--- mobile_app/src/screen/Login/index.js
+++ mobile_app/src/screen/Login/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, TextInput, Button } from "react-native";
+import { Text, View, StyleSheet, TextInput, Button, Image } from "react-native";
+import Logo from '../../assets';
 
 function Login(props) {
   const [username, setUsername] = React.useState("");
@@ -7,18 +8,29 @@ function Login(props) {
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Sign in" onPress={() => signIn({ username, password })} />
+      <View style={styles.loginView}>
+        <View>
+          <Image source={Logo} style={{width: 200, height: 200}} />
+        </View>
+        <TextInput
+          placeholder="Username"
+          value={username}
+          style={{ width: "100%" }}
+          style={styles.input}
+          onChangeText={setUsername}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          style={styles.input}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <Button
+          title="Sign in"
+          onPress={() => signIn({ username, password })}
+        />
+      </View>
     </View>
   );
 }
@@ -26,22 +38,28 @@ function Login(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#003f5c",
+    backgroundColor: "#ccc",
     alignItems: "center",
     justifyContent: "center",
   },
-  inputView: {
-    width: "80%",
-    backgroundColor: "#465881",
-    borderRadius: 25,
-    height: 50,
-    marginBottom: 20,
-    justifyContent: "center",
-    padding: 20,
+  loginView: {
+    // maxWidth: 600,
+    // borderWidth: 1,
+    // borderColor: "#000",
+    padding: 10,
   },
   input: {
-    height: 50,
-    color: "white",
+    marginBottom: 10,
+    width: 250,
+    backgroundColor: "#fff",
+    padding: 5,
+    fontSize: 18,
+    // width: '100%'
+  },
+  image: {
+    resizeMode: 'contain',
+    width: '100%',
+    height: '100%',
   },
 });
 
