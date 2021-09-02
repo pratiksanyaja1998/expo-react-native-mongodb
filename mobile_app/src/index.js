2f9ae7c2b8daff61228e431e9be3d01a7af7b4ac
Pratik Sanyaja <pratiksanyaja1998@gmail.com>
2021-09-02 08:30:50 +0530

    remove header

--- mobile_app/src/index.js
+++ mobile_app/src/index.js
@@ -23,7 +23,7 @@ export default function App(props) {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         {!isSignedIn && <Stack.Screen name="Login" component={LoginScreen} />}
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="SignUp" component={SignupScreen} />
