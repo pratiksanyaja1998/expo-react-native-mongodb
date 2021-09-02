3ecd867166dc7233d1cb929a80d5da985dc6c210
Pratik Sanyaja <pratiksanyaja1998@gmail.com>
2021-09-02 08:14:38 +0530

    fix error

--- mobile_app/src/screen/Login/index.js
+++ mobile_app/src/screen/Login/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 function Login(props) {
   const [username, setUsername] = React.useState("");
